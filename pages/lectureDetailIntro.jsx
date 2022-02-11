5744767104e46122c09de0bbd488731970e62924
jaebong8 <jaebong8@github.com>
2022-02-11 21:40:23 +0900

    lectureDetailIntro page

--- pages/lectureDetailIntro.jsx
+++ pages/lectureDetailIntro.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./lectureDetailIntro.module.scss";
 import TopMenu from "../components/lectureDetailIntro/TopMenu";
+import LectureImage from "../components/lectureDetailIntro/LectureImage";
+import LectureTitle from "../components/lectureDetailIntro/LectureTitle";
+import BottomNavBar from "../components/lectureDetailIntro/BottomNavBar";
 
 function lectureDetailIntro() {
   return (
     <section className={styles.container}>
       <TopMenu />
+      <LectureImage />
+      <LectureTitle />
+      <BottomNavBar />
     </section>
   );
 }
