6fbccbbcf47e360e6e1e334c448ee0bcfc676029
jaebong8 <jaebong8@github.com>
2022-02-14 12:29:37 +0900

    mypageWishList publishing

--- components/lectureDetailIntro/TopMenu.jsx
+++ components/lectureDetailIntro/TopMenu.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./TopMenu.module.scss";
 function TopMenu() {
   return (
     <div className={styles.container}>
-      <img
-        src={"/images/lecture_detail_intro/icon_뒤로가기.svg"}
+      <Image
+        src={"/images/lecture_detail_intro/icon_back.svg"}
         width="24px"
         height="24px"
       />
       <div>
-        <img
+        <Image
           src={"/images/lecture_detail_intro/search.svg"}
           width="24px"
           height="24px"
           className={styles.search}
         />
-        <img
+        <Image
           src={"/images/lecture_detail_intro/logo.svg"}
           width="24px"
           height="24px"
