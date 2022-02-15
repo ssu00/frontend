1d765f12bc31bbe562833846fe4e3f5c96f43493
jaebong8 <jaebong8@github.com>
2022-02-15 14:46:46 +0900

    mypageNotice

---
+++ components/mypageNotice/List.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from "./List.module.scss";
+import Image from "next/image";
+function List() {
+  return (
+    <>
+      <div className={styles.line}></div>
+      <ul className={styles.container}>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+        <li>
+          <span className={styles.date}>2021.09.29</span>
+          <h6>[1.1.0] 업데이트 안내</h6>
+          <span className={styles.tagIcon}>
+            <Image src={"/images/tag_icon2.svg"} width="7px" height="11px" />
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default List;
