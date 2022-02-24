047c8da56f63c0eb80aa66b2083f3733e4ab37d3
ssu00 <rachel4535@naver.com>
2022-02-24 21:45:27 +0900

    🎨 make icon components for common styles

---
+++ components/common/icons/tab_icons.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+const IC_Home = ({ w, h, num }) => {
+  return num == 0 ? (
+    <Image src={"/icons/home.svg"} width={w} height={h} />
+  ) : (
+    <Image src={"/icons/home_select.svg"} width={w} height={h} />
+  );
+};
+
+const IC_Board = ({ w, h, num }) => {
+  return num == 0 ? (
+    <Image src={"/icons/board.svg"} width={w} height={h} />
+  ) : (
+    <Image src={"/icons/board_select.svg"} width={w} height={h} />
+  );
+};
+
+const IC_Chat = ({ w, h, num }) => {
+  return num == 0 ? (
+    <Image src={"/icons/chat.svg"} width={w} height={h} />
+  ) : (
+    <Image src={"/icons/chat_select.svg"} width={w} height={h} />
+  );
+};
+
+const IC_MyPage = ({ w, h, num }) => {
+  return num == 0 ? (
+    <Image src={"/icons/mypage.svg"} width={w} height={h} />
+  ) : (
+    <Image src={"/icons/mypage_select.svg"} width={w} height={h} />
+  );
+};
+
+export { IC_Home, IC_Board, IC_Chat, IC_MyPage };
