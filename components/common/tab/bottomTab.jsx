8d54e3c21105b1c0e7401cc1507713ae54c9d5e6
ssu00 <rachel4535@naver.com>
2022-03-03 00:00:02 +0900

    🚚 move path

--- components/common/tab/bottomTab.jsx
+++ components/common/tab/bottomTab.jsx
@@ -5,7 +5,7 @@ import BottomTabElem from "./bottomTabElement";
 const BottomTab = ({ num }) => {
   return (
     <ul className={styles.tab}>
-      <BottomTabElem url={"/mentor/myClassList"} text={"홈"}>
+      <BottomTabElem url={"/mentor/myclass/myClassList"} text={"홈"}>
         <IC_Home w={24} h={24} num={num[0]} />
       </BottomTabElem>
       <BottomTabElem url={"/board"} text={"자유게시판"}>
@@ -22,85 +22,3 @@ const BottomTab = ({ num }) => {
 };
 
 export default BottomTab;
-
-// import Image from "next/image";
-// import styles from "./bottomtab.module.scss";
-// import router from "next/router";
-
-// const BottomTab = ({ num }) => {
-//   return (
-//     <>
-//       <ul className={styles.tab}>
-//         <button
-//           type="button"
-//           onClick={() => router.push("/myclass")}
-//           className={styles.elements}
-//         >
-//           <li>
-//             <div className={styles.img}>
-//               <Image
-//                 src={
-//                   num == 1
-//                     ? "/newImage/bottom_home_blue.svg"
-//                     : "/newImage/bottom_home.svg"
-//                 }
-//                 width="24px"
-//                 height="24px"
-//                 margin="0"
-//                 alt="홈"
-//                 className={styles.img}
-//               />
-//             </div>
-//             <span>홈</span>
-//           </li>
-//         </button>
-
-//         <button
-//           type="button"
-//           onClick={() => router.push("/chatLists")}
-//           className={styles.elements}
-//         >
-//           <li>
-//             <div className={styles.img}>
-//               <Image
-//                 src={
-//                   num == 2
-//                     ? "/newImage/bottom_chat_blue.svg"
-//                     : "/newImage/bottom_chat.svg"
-//                 }
-//                 width="24px"
-//                 height="24px"
-//                 alt="채팅리스트"
-//                 className={styles.img}
-//               />
-//             </div>
-//             <span>채팅</span>
-//           </li>
-//         </button>
-
-//         <button
-//           type="button"
-//           onClick={() => router.push("/mypage")}
-//           className={styles.elements}
-//         >
-//           <li>
-//             <div className={styles.img}>
-//               <Image
-//                 src={
-//                   num == 3
-//                     ? "/newImage/bottom_mypage_blue.svg"
-//                     : "/newImage/bottom_mypage.svg"
-//                 }
-//                 width="24px"
-//                 height="24px"
-//                 alt="마이페이지"
-//               />
-//             </div>
-//             <span>마이페이지</span>
-//           </li>
-//         </button>
-//       </ul>
-//     </>
-//   );
-// };
-// export default BottomTab;
