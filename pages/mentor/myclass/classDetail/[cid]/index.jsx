ea826717defce8821441cba78a23b22961381abc
ssu00 <rachel4535@naver.com>
2022-04-02 16:36:05 +0900

    💄 Style: 멘토 - sample 이미지를 강의 이미지로 변경

--- pages/mentor/myclass/classDetail/[cid]/index.jsx
+++ pages/mentor/myclass/classDetail/[cid]/index.jsx
@@ -42,7 +42,13 @@ const ClassDetail = ({ token, classData, reviewData }) => {
     <section className={styles.classDetailSection}>
       <TopBar onClick={() => router.push("/mentor/myclass/myClassList")} />
       <div className={styles.imageBlock}>
-        <Image src={"/samples/lecture.png"} width={375} height={277} />
+        <Image
+          src={
+            classData.thumbnail ? classData.thumbnail : "/samples/lecture.png"
+          }
+          width={375}
+          height={277}
+        />
         <div className={styles.edit_remove_btn}>
           <BasicBtn
             text={"수정"}
