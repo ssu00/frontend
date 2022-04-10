05caa8f707fa4679d47edb240074b3eef8b44939
ssu00 <rachel4535@naver.com>
2022-04-10 19:59:38 +0900

    ⚡️ add notification page

--- components/mentor/mypage/mypageTopBar.jsx
+++ components/mentor/mypage/mypageTopBar.jsx
@@ -1,12 +1,17 @@
 import styles from "./mypageTopBar.module.scss";
 import { IC_AlarmActive, IC_Setting, IC_Menu } from "../../../icons";
+import router from "next/router";
 
 const MyPageTopBar = () => {
   return (
     <section className={styles.mypageTopBar}>
       <h1 className={styles.title}>마이페이지</h1>
       <div className={styles.btns}>
-        <button type="button" className={styles.mypageBtn}>
+        <button
+          type="button"
+          className={styles.mypageBtn}
+          onClick={() => router.push("/common/notification")}
+        >
           <IC_AlarmActive />
         </button>
         <button type="button" className={styles.mypageBtn}>
