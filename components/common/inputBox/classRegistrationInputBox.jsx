f543db7cea8b18ee0f31fa51df30614bd83f1765
ssu00 <rachel4535@naver.com>
2022-04-29 09:38:04 +0900

    ⚡️ Feat: 강의 등록 시 글자 수 제한 기능 추가

--- components/common/inputBox/classRegistrationInputBox.jsx
+++ components/common/inputBox/classRegistrationInputBox.jsx
@@ -1,12 +1,15 @@
 import styles from "./classRegistrationInputBox.module.scss";
-const ClassRegistrationInputBox = ({ placeholder, value, onChange }) => {
+const ClassRegistrationInputBox = ({ placeholder, value, onChange, limit }) => {
   return (
-    <textarea
-      className={styles.classRegistrationInputBox}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
+    <div className={styles.classRegistrationInputBox}>
+      <textarea
+        className={styles.inputBox}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <h1 className={styles.textLimit}>{limit}</h1>
+    </div>
   );
 };
 
