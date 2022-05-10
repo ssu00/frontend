386c9aba909d6c85b6cd48ac0418f21c431f0d55
ssu00 <rachel4535@naver.com>
2022-05-10 20:25:02 +0900

    🧪 Test: 쿠키 테스트

--- core/api/Login/login.jsx
+++ core/api/Login/login.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 const Login_API = async (id, pw) => {
   try {
     const res = await axios.post("/login", { password: pw, username: id });
+    console.log("login res==", res);
     return res;
   } catch (err) {
     return err;
