ff65d414362a64f6fbdc70a9e01e095a37bf61ef
Jetom88 <shinhro3o@gmail.com>
2022-05-11 15:25:31 +0900

    💄 Design: 문의하기 css 수정 및 basicSelectBox 컴포넌트 className 추가

--- components/common/inputBox/basicSelectBox.jsx
+++ components/common/inputBox/basicSelectBox.jsx
@@ -1,9 +1,21 @@
 import { IC_ArrowDoubleTriangle } from "../../../icons";
 import styles from "./basicSelectBox.module.scss";
-const BasicSelectBox = ({ arr, name, onChange }) => {
+import classNames from "classnames";
+const BasicSelectBox = ({
+  arr,
+  name,
+  onChange,
+  otherClassName,
+  selectStyles,
+}) => {
   return (
-    <div className={styles.selectBox}>
-      <select name={name} id={name} onChange={onChange}>
+    <div className={classNames(styles.selectBox, otherClassName)}>
+      <select
+        name={name}
+        id={name}
+        onChange={onChange}
+        className={selectStyles}
+      >
         {arr?.map((data, i) => (
           <option value={data} key={i}>
             {data}
