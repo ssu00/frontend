bc9debd5496842767a81db82c9d17c2949f03e16
ssu00 <rachel4535@naver.com>
2022-05-23 21:32:26 +0900

    ✏️ Comment: 코드 주석 처리

--- components/mentee/header.jsx
+++ components/mentee/header.jsx
@@ -28,12 +28,12 @@ function Header() {
       <IC_LogoText className={styles.logo} onClick={handleClickLogo} />
       <Dropdown options={["서울시 관악구", "서울시 동작구"]} />
       <div className={styles.rightPannel}>
-        <button aria-label="알람 확인">
+        {/* <button aria-label="알람 확인">
           <IC_Alarm />
         </button>
         <button aria-label="메뉴 열기">
           <IC_Menu />
-        </button>
+        </button> */}
       </div>
     </header>
   );
