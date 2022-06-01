210e021de832ea89ba3e8ed5e384b452e2c43d54
Salvation-sub <kyman19940214@gmail.com>
2022-06-02 07:09:19 +0900

    Feat: 신청한 강의 리스트  승인예정/승인완료 추가

--- pages/mentee/mypage/mypageRegisteredLecture.jsx
+++ pages/mentee/mypage/mypageRegisteredLecture.jsx
@@ -3,20 +3,34 @@ import styles from "./mypageRegisteredLecture.module.scss";
 import router from "next/router";
 import { BottomTab, TopBar } from "../../../components/common";
 import LectureBlock from "../../../components/mentee/myRegisteredLecture/LectureBlock";
-import { getRegisteredLectures } from "../../../core/api/Mentee";
+
 import NoWrite from "../../../components/mentee/NoWrite";
+import {
+  getApprovedLectures,
+  getNotApprovedLectures,
+} from "../../../core/api/Mentee/getRegisteredLectures";
 
 export const getServerSideProps = async (context) => {
   const token = cookie.parse(context.req.headers.cookie).accessToken;
   const role = cookie.parse(context.req.headers.cookie).role;
-  const lectures = await getRegisteredLectures(token, 1);
+  const notApprovedLectures = await getNotApprovedLectures(token, 1);
+
+  const approvedLectures = await getApprovedLectures(token, 1);
 
   return {
-    props: { token, lectures: JSON.parse(JSON.stringify(lectures)), role },
+    props: { token, notApprovedLectures, approvedLectures, role },
   };
 };
 
-const MypageRegisteredLecture = ({ token, lectures, role }) => {
+const MypageRegisteredLecture = ({
+  token,
+  notApprovedLectures,
+  approvedLectures,
+  role,
+}) => {
+  const date = new Date();
+  const today = `${date.getFullYear()}.${date.getMonth()}.${date.getDay()}`;
+
   return (
     <section className={styles.LectureListSection}>
       <TopBar text={"구매한 강의"} onClick={() => router.back()} />
@@ -29,16 +43,37 @@ const MypageRegisteredLecture = ({ token, lectures, role }) => {
       <div className={styles.line} />
 
       <div className={styles.finished}>
-        {/* <div className={styles.titleBox}>
-          <h1 className={styles.title}>2021.07.01</h1>
-          <IC_ArrowRight />
-        </div> */}
-        {lectures.content.length !== 0 ? (
-          lectures.content.map((lecture, idx) => (
-            <LectureBlock key={idx} lecture={lecture} />
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>{today}</h1>
+          <div className={styles.approveChip}>승인 예정</div>
+        </div>
+        {notApprovedLectures.content.length !== 0 ? (
+          notApprovedLectures.content.map((lecture, idx) => (
+            <LectureBlock key={idx} lecture={lecture} approved={false} />
           ))
         ) : (
-          <NoWrite text={"구매한 강의가 없습니다"} />
+          <NoWrite text={"구매한 강의가 없습니다."} />
+        )}
+      </div>
+
+      <div className={styles.line} />
+
+      <div className={styles.finished}>
+        <div className={styles.titleBox}>
+          <h1 className={styles.title}>{today}</h1>
+          <div className={styles.approvedChip}>승인 완료</div>
+        </div>
+        {approvedLectures.content.length !== 0 ? (
+          approvedLectures.content.map((lecture, idx) => (
+            <LectureBlock
+              key={idx}
+              lecture={lecture}
+              approved={true}
+              token={token}
+            />
+          ))
+        ) : (
+          <NoWrite text={"승인 완료된 강의가 없습니다."} />
         )}
       </div>
 
